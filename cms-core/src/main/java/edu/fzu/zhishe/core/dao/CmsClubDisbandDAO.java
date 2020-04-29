ab0791c97b9dc99ca5ad8aa039aedebdf449866f
yz990219 <50985454+yz990219@users.noreply.github.com>
2020-04-29 22:38:10 +0800

    fix(club):delete two blank file

--- cms-core/src/main/java/edu/fzu/zhishe/core/dao/CmsClubDisbandDAO.java
+++
@@ -1,9 +0,0 @@
-package edu.fzu.zhishe.core.dao;
-
-import edu.fzu.zhishe.core.dto.CmsClubsDisbandReturnParam;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
-
-public interface CmsClubDisbandDAO {
-    List<CmsClubsDisbandReturnParam> listClubDisbandApply(@Param("queryParam") CmsClubsDisbandReturnParam cmsClubsDisbandReturnParam);
-}
